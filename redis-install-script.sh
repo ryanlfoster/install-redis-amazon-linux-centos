45340f286288811af16e3c48ab7bb69998278cb5
Jorge Rodríguez <rcabrera.jorge@gmail.com>
2012-11-19 16:42:53 +0000

    Adjust script for running as root

--- redis-install-script.sh
+++ redis-install-script.sh
@@ -11,24 +11,25 @@
 echo "*****************************************"
 echo " 1. Prerequisites: Install updates, set time zones, install GCC and make"
 echo "*****************************************"
-sudo yum -y update
-sudo ln -sf /usr/share/zoneinfo/America/Indianapolis /etc/localtime
-sudo yum -y install gcc gcc-c++ make 
+yum -y update
+ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+yum -y install gcc gcc-c++ make 
 echo "*****************************************"
-echo " 2. Download, Untar and Make Redis 2.6"
+echo " 2. Download, Untar and Make Redis 2.6.4"
 echo "*****************************************"
-sudo wget http://redis.googlecode.com/files/redis-2.6.0-rc3.tar.gz
-sudo tar xzf redis-2.6.0-rc3.tar.gz
-sudo rm redis-2.6.0-rc3.tar.gz -f
-cd redis-2.6.0-rc3
-sudo make
-sudo make install
+wget http://redis.googlecode.com/files/redis-2.6.4.tar.gz
+tar xzf redis-2.6.4.tar.gz
+rm -f redis-2.6.4.tar.gz
+cd redis-2.6.4
+make
+make install
 echo "*****************************************"
 echo " 3. Create Directories and Copy Redis Files"
 echo "*****************************************"
-sudo mkdir /etc/redis /var/lib/redis
-sudo cp src/redis-server src/redis-cli /usr/local/bin
-sudo cp redis.conf /etc/redis
+rm -rf /etc/redis /var/lib/redis
+mkdir /etc/redis /var/lib/redis
+cp src/redis-server src/redis-cli /usr/local/bin
+cp redis.conf /etc/redis
 echo "*****************************************"
 echo " 4. Configure Redis.Conf"
 echo "*****************************************"
@@ -39,7 +40,7 @@ echo " 3: ... dir /var/lib/redis"
 echo " 4: ... loglevel notice"
 echo " 5: ... logfile /var/log/redis.log"
 echo "*****************************************"
-sudo sed -e "s/^daemonize no$/daemonize yes/" -e "s/^# bind 127.0.0.1$/bind 127.0.0.1/" -e "s/^dir \.\//dir \/var\/lib\/redis\//" -e "s/^loglevel verbose$/loglevel notice/" -e "s/^logfile stdout$/logfile \/var\/log\/redis.log/" redis.conf > /etc/redis/redis.conf
+sed -e "s/^daemonize no$/daemonize yes/" -e "s/^# bind 127.0.0.1$/bind 127.0.0.1/" -e "s/^dir \.\//dir \/var\/lib\/redis\//" -e "s/^loglevel verbose$/loglevel notice/" -e "s/^logfile stdout$/logfile \/var\/log\/redis.log/" redis.conf > /etc/redis/redis.conf
 echo "*****************************************"
 echo " 5. Download init Script"
 echo "*****************************************"
@@ -47,17 +48,17 @@ wget https://raw.github.com/saxenap/install-redis-amazon-linux-centos/master/red
 echo "*****************************************"
 echo " 6. Move and Configure Redis-Server"
 echo "*****************************************"
-sudo mv redis-server /etc/init.d
-sudo chmod 755 /etc/init.d/redis-server
+mv redis-server /etc/init.d
+chmod 755 /etc/init.d/redis-server
 echo "*****************************************"
 echo " 7. Auto-Enable Redis-Server"
 echo "*****************************************"
-sudo chkconfig --add redis-server
-sudo chkconfig --level 345 redis-server on
+chkconfig --add redis-server
+chkconfig --level 345 redis-server on
 echo "*****************************************"
 echo " 8. Start Redis Server"
 echo "*****************************************"
-sudo service redis-server start
+service redis-server start
 echo "*****************************************"
 echo " Installation Complete!"
 echo " You can test your redis installation using the redis console:"
@@ -71,6 +72,5 @@ echo " 2: ... bind 127.0.0.1"
 echo " 3: ... dir /var/lib/redis"
 echo " 4: ... loglevel notice"
 echo " 5: ... logfile /var/log/redis.log"
+echo " INSTALLATION FINISHED SUCCESSFULLY"
 echo "*****************************************"
-read -p "Press [Enter] to continue..."
-
